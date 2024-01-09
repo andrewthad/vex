fe1e30c4222369ee993e183ecc9b7fc25757562a
Andrew Martin <andrew.thaddeus@gmail.com>
2024-01-09 16:48:37 -0500

    Switch from GHC < 9.4 to GHC >= 9.4

--- src/Vector/MutableUnliftedArray.hs
+++ src/Vector/MutableUnliftedArray.hs
@@ -30,20 +30,23 @@ module Vector.MutableUnliftedArray
 
 import Prelude hiding (read,length)
 
+import Data.Primitive.Unlifted.Class (Unlifted)
 import Arithmetic.Unsafe (Nat(Nat),type (<)(Lt))
 import Data.Kind (Type)
-import GHC.Exts (Int(I#))
+import GHC.Exts (Int(I#),Array#,MutableArray#)
 import GHC.ST (ST(ST))
-import Data.Primitive.Unlifted.Array (MutableUnliftedArray(..))
+import Data.Primitive.Unlifted.Array (MutableUnliftedArray,MutableUnliftedArray_(MutableUnliftedArray))
+import Data.Primitive.Unlifted.Array.Primops (MutableUnliftedArray#(..))
 
 import qualified GHC.Exts as Exts
 import qualified GHC.TypeNats as GHC
+import qualified Data.Primitive.Unlifted.Array.Primops as Primops
 
 data Vector :: Type -> GHC.Nat -> Type -> Type where
-  Vector :: Exts.ArrayArray# -> Vector s n a
+  Vector :: Array# (MutableArray# s (Unlifted a)) -> Vector s n a
 
 data MutableVector :: Type -> GHC.Nat -> Type -> Type where
-  MutableVector :: Exts.MutableArrayArray# s -> MutableVector s n a
+  MutableVector :: MutableArray# s (MutableArray# s (Unlifted a)) -> MutableVector s n a
 
 -- | Create an array of uninitialized memory. This is
 -- exceptionally unsafe when working with a vector of @MutableByteArray@
@@ -52,18 +55,18 @@ data MutableVector :: Type -> GHC.Nat -> Type -> Type where
 uninitialized :: Nat n -> ST s (MutableVector s n a)
 {-# inline uninitialized #-}
 uninitialized (Nat (I# sz)) = ST
-  ( \s0 -> case Exts.newArrayArray# sz s0 of
-    (# s1, arr #) -> (# s1, MutableVector arr #)
+  ( \s0 -> case Primops.unsafeNewUnliftedArray# sz s0 of
+    (# s1, MutableUnliftedArray# arr #) -> (# s1, MutableVector arr #)
   )
 
 index ::
      (m < n) -- ^ Evidence the index is in-bounds
-  -> Vector s n b -- ^ Array
+  -> Vector s n a -- ^ Array
   -> Nat m -- ^ Index
   -> MutableUnliftedArray s a
 {-# inline index #-}
-index Lt (Vector arr) (Nat (I# i)) =
-  MutableUnliftedArray (Exts.unsafeCoerce# (Exts.indexArrayArrayArray# arr i))
+index Lt (Vector arr) (Nat (I# i)) = case Exts.indexArray# arr i of
+  (# r #) -> MutableUnliftedArray (MutableUnliftedArray# r)
 
 read ::
      (m < n) -- ^ Evidence the index is in-bounds
@@ -73,8 +76,8 @@ read ::
 {-# INLINE read #-}
 -- this is a core operation
 read Lt (MutableVector arr) (Nat (I# i)) = ST
-  (\s0 -> case Exts.readMutableArrayArrayArray# arr i s0 of
-    (# s1, r #) -> (# s1, MutableUnliftedArray r #)
+  (\s0 -> case Exts.readArray# arr i s0 of
+    (# s1, r #) -> (# s1, MutableUnliftedArray (MutableUnliftedArray# r) #)
   )
 
 write ::
@@ -85,14 +88,14 @@ write ::
   -> ST s ()
 {-# INLINE write #-}
 -- this is a core operation
-write Lt (MutableVector arr) (Nat (I# i)) (MutableUnliftedArray x) = ST
-  (\s0 -> (# Exts.writeMutableArrayArrayArray# arr i x s0, () #))
+write Lt (MutableVector arr) (Nat (I# i)) (MutableUnliftedArray (MutableUnliftedArray# x)) = ST
+  (\s0 -> (# Exts.writeArray# arr i x s0, () #))
 
 unsafeFreeze ::
      MutableVector s n a
   -> ST s (Vector s n a)
 {-# inline unsafeFreeze #-}
 unsafeFreeze (MutableVector marr) = ST
-  (\s0 -> case Exts.unsafeFreezeArrayArray# marr s0 of
+  (\s0 -> case Exts.unsafeFreezeArray# marr s0 of
     (# s1, arr #) -> (# s1, Vector arr #)
   )
